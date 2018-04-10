2f80498ab4d54723fc0429b31442c03474f7fe43
alin <alinyo.mail@gmail.com>
2018-04-10 17:09:17 +0800

    Add pre install script

---
+++ gateway/install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "Environment installation"
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install vim nodejs
+
+sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev
+
+cd /home/pi/server/
+npm install
+
+echo "install done"
+
+exit 0
